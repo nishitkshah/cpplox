dd48be5bdd46b6384519732c9d866499a69281f7
nishitkshah <40242169+nishitkshah@users.noreply.github.com>
2019-08-22 23:31:57 +0530

    Added Expr anbstract class for AST (Abstract Syntax Tree)

---
+++ includes/expr.hpp
@@ -0,0 +1,13 @@
+// Reference: http://www.craftinginterpreters.com/
+
+#ifndef EXPR_H
+#define EXPR_H
+
+namespace lox {
+
+    class Expr {    // Abstract class for Abstract Syntax Tree
+    };
+
+} // namespace lox
+
+#endif
